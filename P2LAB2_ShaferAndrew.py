eb060edeb93bf90c4d44631e19dddd473afdeba7
shafera3641 <113615092+shafera3641@users.noreply.github.com>
2022-12-08 06:56:52 -0800

    Add files via upload
    P2LAB2 Simple Statistics

---
+++ P2LAB2_ShaferAndrew.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+your_value = ((num1 * num2 * num3 * num4), (num1 + num2 + num3 + num4) /4)
+
+print(f'{your_value[0]:.0f} {your_value[1]:.0f}')
+
+print(f'{your_value[0]:.3f} {your_value[1]:.3f}')
